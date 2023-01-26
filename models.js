17a2e1f4b58598bf5bb451cb2629bb57bdd4561e
Александр <106553458+alenichev1@users.noreply.github.com>
2023-01-26 21:36:02 +0300

    Create models.js

---
+++ models.js
@@ -0,0 +1,76 @@
+const sequelize = require("../db");
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: "USER" },
+});
+
+const Basket = sequelize.define("basket", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const BasketFlowers = sequelize.define("basket_flowers", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const Flowers = sequelize.define("flowers", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+  price: { type: DataTypes.INTEGER, allowNull: false },
+  img: { type: DataTypes.STRING, allowNull: false },
+});
+
+const Type = sequelize.define("type", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const Brand = sequelize.define("brand", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const FlowersInfo = sequelize.define("flowers_info", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING, allowNull: false },
+});
+
+const TypeBrand = sequelize.define("type_brand", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+User.hasOne(Basket);
+Basket.belongsTo(User);
+
+Basket.hasMany(BasketFlowers);
+BasketFlowers.belongsTo(Basket);
+
+Type.hasMany(Flowers);
+Flowers.belongsTo(Type);
+
+Brand.hasMany(Flowers);
+Flowers.belongsTo(Brand);
+
+Flowers.hasMany(BasketFlowers);
+BasketFlowers.belongsTo(Flowers);
+
+Flowers.hasMany(FlowersInfo, { as: "info" });
+FlowersInfo.belongsTo(Flowers);
+
+Type.belongsToMany(Brand, { through: TypeBrand });
+Brand.belongsToMany(Type, { through: TypeBrand });
+
+module.exports = {
+  User,
+  Basket,
+  BasketFlowers,
+  Flowers,
+  Type,
+  Brand,
+  TypeBrand,
+  FlowersInfo,
+};
